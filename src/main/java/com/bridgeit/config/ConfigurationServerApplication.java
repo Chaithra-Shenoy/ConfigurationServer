b3c4c8cf79f233be870c5b2afda905ffa659ecdf
Chaithra-Shenoy <chaithrashenoy08@gmail.com>
2018-08-18 11:04:57 +0530

    18/08/2018 Configuration server program updated

--- src/main/java/com/bridgeit/config/ConfigurationServerApplication.java
+++ src/main/java/com/bridgeit/config/ConfigurationServerApplication.java
@@ -1,16 +1,22 @@
 package com.bridgeit.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 @SpringBootApplication
 @EnableConfigServer
 @EnableDiscoveryClient
 public class ConfigurationServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigurationServerApplication.class, args);
 	}
+	
+	
 }
